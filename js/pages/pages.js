3a99ecb82bad4d4385d0c81b679a910bb90a2f66
MTkhai <kcott.maitrungkhai@gmail.com>
2026-06-16 09:22:14 +0000

    Jun 16, 2026, 4:22 PM

--- js/pages/pages.js
+++ js/pages/pages.js
@@ -1,5 +1,5 @@
 import{state} from "../store/state.js";
-import { collection, addDoc, onSnapshot, query, where, orderBy, doc, updateDoc, db } from "../config/firebase-config.js";
+import { collection, addDoc,deleteDoc, onSnapshot, query, where, orderBy, doc, updateDoc, db } from "../config/firebase-config.js";
 import { getRelativeTime } from "../utils/helpers.js";
 
 // ================= 8. FIRESTORE SYNC & RENDER =================
